2fb0046bf514567e6e7c58992d88a29657d1130f
burnstick <zimingmaosu@gmail.com>
2020-04-12 21:51:34 -0400

    hhh

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const port = process.env.port || 3000;
 
 app.get('/', (req, res) => {
     res.send("Welcome sb.");
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("SB!");
 });
 
